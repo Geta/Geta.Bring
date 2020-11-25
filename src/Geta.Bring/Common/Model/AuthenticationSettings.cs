99339599e5584d13b98e722f724670a21f0e59c2
Sven-Erik Jonsson <sven.erik@geta.no>
2020-11-25 18:58:10 +0100

    Authentication for Pickup api.

---
+++ src/Geta.Bring/Common/Model/AuthenticationSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Geta.Bring.Common.Model
+{
+    public class AuthenticationSettings : IAuthenticationSettings
+    {
+        public Uri ClientUri { get; set; }
+
+        public string Uid { get; set; }
+
+        public string Key { get; set; }
+    }
+}
--- src/Geta.Bring/Pickup/PickupSettings.cs
+++ src/Geta.Bring/Pickup/PickupSettings.cs
@@ -1,28 +1,50 @@
-﻿using System;
+﻿using Geta.Bring.Common.Model;
+using System;
 
 namespace Geta.Bring.Pickup
 {
     /// <summary>
     /// Settings for <see cref="PickupClient" /> 
     /// </summary>
-    public class PickupSettings
+    public class PickupSettings : IAuthenticationSettings
     {
         // <summary>
         /// Initializes new instance of <see cref="PickupSettings"/> with default Bring Shipping Guide API endpoint: https://api.bring.com/pickuppoint/api/pickuppoint/
         /// </summary>
         /// <param name="clientUri">The URI of client Web site.</param>
-        public PickupSettings()
-            : this(new Uri("https://api.bring.com/pickuppoint/api/pickuppoint/")) { }
-
-
+        /// <param name="uid">required MyBring API User ID.</param>
+        /// <param name="key">required MyBring API Key.</param>
+        /// <remarks>Register at https://www.mybring.com/ for an api key</remarks>
         public PickupSettings(
-           Uri endpointUri)
-        {
-            if (endpointUri == null) throw new ArgumentNullException(nameof(endpointUri));
+            Uri clientUri,
+            string uid = null,
+            string key = null)
+            : this(clientUri, new Uri("https://api.bring.com/pickuppoint/api/pickuppoint/"), uid, key) { }
 
-            EndpointUri = endpointUri;
+        /// <summary>
+        /// Initializes new instance of <see cref="PickupSettings"/>
+        /// </summary>
+        /// <param name="clientUri">The URI of client Web site.</param>
+        /// <param name="endpointUri">The URI of Bring Shipping Guide API endpoint.</param>
+        /// <param name="uid">required MyBring API User ID.</param>
+        /// <param name="key">required MyBring API Key.</param>
+        /// <remarks>Register at https://www.mybring.com/ for an api key</remarks>
+        public PickupSettings(
+            Uri clientUri,
+            Uri endpointUri,
+            string uid = null,
+            string key = null)
+        {
+            EndpointUri = endpointUri ?? throw new ArgumentNullException(nameof(endpointUri));
+            ClientUri = clientUri ?? throw new ArgumentNullException(nameof(clientUri));
+            Uid = uid;
+            Key = key;
         }
 
+        public Uri ClientUri { get; }
         public Uri EndpointUri { get; }
+
+        public string Uid { get; }
+        public string Key { get; }
     }
 }
